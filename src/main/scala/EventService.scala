d473ed226b7e1f1033224bb520af4bdd10440784
Adam Rosien <adam.rosien@underscore.io>
2020-04-24 08:15:04 -0700

    Make EventService an App.

--- src/main/scala/EventService.scala
+++ src/main/scala/EventService.scala
@@ -1,11 +1,8 @@
 package io.innerproduce
 package nba
 
-object EventService {
-  def run(): Unit = {
-    // 1. Read events until there are no more.
-    // 2. Collate a BoxScore from the events
-    // 3. Post the BoxScore to the BoxScoreService, ensuring there are no errors.
-    ()
-  }
+object EventService extends App {
+  // 1. Read events until there are no more.
+  // 2. Collate a BoxScore from the events.
+  // 3. Post the BoxScore to the BoxScoreService, ensuring there are no errors.
 }
